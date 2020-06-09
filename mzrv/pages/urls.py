bb40dd7ef364c8fb72be51ae79ea8fb780c6b52e
Thiago Assis <thiago.medassis@gmail.com>
2020-06-09 17:12:27 -0300

    Listagem simples passando conteúdo para os includes

--- mzrv/pages/urls.py
+++ mzrv/pages/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('games', views.games, name='games'),
+    path('consoles', views.consoles, name='consoles'),
+    path('acessories', views.acessories, name='acessories'),
 ]
--- mzrv/pages/views.py
+++ mzrv/pages/views.py
@@ -2,4 +2,73 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    extra_context = {
+        'page': 'Jogos',
+        'games': [
+            {
+                'img': 'https://via.placeholder.com/150',
+                'title': 'Red Dead Redemption 2',
+                'price': '199.99',
+                'url': '/games/1'
+            }
+        ],
+        'consoles': [
+            {
+                'img': 'https://via.placeholder.com/150',
+                'title': 'Playstation 4',
+                'price': '1899.99',
+                'url': '/consoles/1'
+            }
+        ],
+        'acessories': [
+            {
+                'img': 'https://via.placeholder.com/150',
+                'title': 'Controle PS4',
+                'price': '159.99',
+                'url': '/acessories/1'
+            }
+        ]
+    }
+    return render(request, 'pages/index.html', extra_context)
+
+def games(request):
+    extra_context = {
+        'page': 'Jogos',
+        'objects': [
+            {
+                'img': 'https://via.placeholder.com/150',
+                'title': 'Red Dead Redemption 2',
+                'price': '199.99',
+                'url': '/games/1'
+            }
+        ]
+    }
+    return render(request, 'pages/listings.html', extra_context)
+
+def consoles(request):
+    extra_context = {
+        'page': 'Videogames',
+        'objects': [
+            {
+                'img': 'https://via.placeholder.com/150',
+                'title': 'Playstation 4',
+                'price': '1899.99',
+                'url': '/consoles/1'
+            }
+        ]
+    }
+    return render(request, 'pages/listings.html', extra_context)
+
+def acessories(request):
+    extra_context = {
+        'page': 'Acessórios',
+        'objects': [
+            {
+                'img': 'https://via.placeholder.com/150',
+                'title': 'Controle PS4',
+                'price': '159.99',
+                'url': '/acessories/1'
+            }
+        ]
+    }
+    return render(request, 'pages/listings.html', extra_context)
