54b04356678b4186aca5825b0961c6211873b1cb
Thiago Assis <thiago.medassis@gmail.com>
2020-06-15 10:43:41 -0300

    Adicioando o modelo de Games e subindo o requirements

---
+++ mzrv/games/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class GamesConfig(AppConfig):
+    name = 'games'
+    verbose_name = 'Gestão de jogos'
+    verbose_plural_name = 'Jogos'
---
+++ mzrv/games/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from django.contrib.postgres.fields import ArrayField
+from django.db import models
+
+
+class Games(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    main_photo = models.ImageField(upload_to='photos/games/')
+    plataform = ArrayField(models.CharField(max_length=200))
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        verbose_name = 'Jogo'
